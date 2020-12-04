8d6bf952e87607ac5e5b0fa74cbd5833106ef3f9
Ryan Tanaka <tanaka@isi.edu>
2020-12-03 18:41:03 -0600

    updated added of files to rc; added OSG specifc run flag

--- Workflow-docker.py
+++ Workflow-docker.py
@@ -3,81 +3,78 @@
 
 import os
 import logging
-from pathlib import Path
-import requests 
-from glob import glob
-from zipfile import ZipFile
 import pickle
+from glob import glob
+from pathlib import Path
+from zipfile import ZipFile
+
+import requests 
 import pandas as pd
 
 logging.basicConfig(level=logging.DEBUG)
 
-#Import Pegasus API
 from Pegasus.api import *
 
-#Properties
+# --- Properties ---------------------------------------------------------------
 props = Properties()
 props["dagman.retry"] = "100"
 props["pegasus.transfer.arguments"] = "-m 1"
 props.write()
 
-#Replica Catalog
+# --- Replica Catalog ----------------------------------------------------------
 rc = ReplicaCatalog()
-input_files = glob('*.jpg')
-input_files.sort()
-in_files=[]
 
+# checkpoint files
 checkpoint_file = "checkpoint_file2.hdf5"
 if not os.path.isfile(checkpoint_file):
     df = pd.DataFrame(list())
     df.to_csv(checkpoint_file)
+
+rc.add_replica("local", checkpoint_file, Path(".").resolve() / checkpoint_file)
     
 hpo_checkpoint_file = 'hpo_checkpoint.pkl'
 if not os.path.isfile(hpo_checkpoint_file):
     df = pd.DataFrame(list())
     df.to_csv(hpo_checkpoint_file)
-    
-for file in input_files:
-    in_files.append(File(file))
-    rc.add_replica("local", File(file), str(Path(".").resolve() / file))  
-rc.add_replica("local", checkpoint_file, Path(".").resolve() / checkpoint_file)
+
 rc.add_replica("local", hpo_checkpoint_file, Path(".").resolve() / hpo_checkpoint_file)
+
+# input images
+input_files = [f for f in Path("./inputs").iterdir() if f.is_file() and f.name.endswith(".jpg")]
+input_files.sort(key=lambda f: f.name.lower())
+in_files=[]
+ 
+for file in input_files:
+    in_files.append(File(file.name))
+    rc.add_replica(site="local", lfn=file.name, pfn=file.resolve())
+
 rc.write()
 
+# --- Transformation Catalog ---------------------------------------------------
+# set to use OSG specific settings
+RUN_ON_OSG = True
 
-#Transformation
-tools_container = Container(
-                    "tools-container", 
-                    Container.DOCKER, 
-                    image="docker:///ssrujanaa/catsanddogs:latest"
-                )
-
-# tools_container = Container(
-#                     "tools-container", 
-#                     Container.DOCKER, 
-#                     image=str(Path(".").resolve() / "tools_cont.tar"),
-#                     image_site="local"
-#                 )
+tools_container = Container("tools-container", Container.DOCKER, image="docker:///ssrujanaa/catsanddogs:latest")
 
 pre_process_resize = Transformation( "preprocess1.py",
             site="condorpool",
             pfn="/usr/bin/preprocess1.py",
             is_stageable=False,
-            container=tools_container
+            container=None if RUN_ON_OSG else tools_container
             )
 
 pre_process_augment = Transformation( "Augmentation.py",
             site="condorpool",
             pfn="/usr/bin/Augmentation.py",
             is_stageable=False,
-            container=tools_container
+            container=None if RUN_ON_OSG else tools_container
             )
 
 data_split  = Transformation( "Data_Split.py",
             site="condorpool",
             pfn="/usr/bin/Data_Split.py",
             is_stageable=False,
-            container=tools_container
+            container=None if RUN_ON_OSG else tools_container
             )
 
 
@@ -85,31 +82,37 @@ hpo  = Transformation( "hpo_checkpointing.py",
             site="condorpool",
             pfn="/usr/bin/hpo_checkpointing.py",
             is_stageable=False,
-            container=tools_container
+            container=None if RUN_ON_OSG else tools_container
             )
 
 vgg_model  = Transformation( "VGG_model.py",
             site="condorpool",
             pfn="/usr/bin/VGG_model.py",
             is_stageable=False,
-            container=tools_container
+            container=None if RUN_ON_OSG else tools_container
             )
 
 test_model =  Transformation( "Test.py",
             site="condorpool",
             pfn="/usr/bin/Test.py",
             is_stageable=False,
-            container=tools_container
+            container=None if RUN_ON_OSG else tools_container
             )
                     
-tc = TransformationCatalog()\
-    .add_containers(tools_container)\
-    .add_transformations(pre_process_resize,pre_process_augment,data_split,hpo,vgg_model,test_model)\
-    .write()
+# add OSG specific settings
+if RUN_ON_OSG:
+    for tr in [pre_process_resize, pre_process_augment, data_split, hpo, vgg_model, test_model]:
+        tr.add_condor_profile(requirements='HAS_SINGULARITY == True')
+        tr.add_profiles(Namespace.CONDOR, key="+SingularityImage", value='"/cvmfs/singularity.opensciencegrid.org/ssrujanaa/catsanddogs:latest"')
+    # add request_gpus for tr that need it
 
-#Workflow
+tc = TransformationCatalog()
+tc.add_containers(tools_container)
+tc.add_transformations(pre_process_resize,pre_process_augment,data_split,hpo,vgg_model,test_model)
+tc.write()
+
+# --- Workflow -----------------------------------------------------------------
 wf = Workflow("Cats_and_Dogs", infer_dependencies=True)
-
 
 resized_images = File('resized_images.txt')
 all_files = [File("resized_{}".format(f.lfn)) for f in in_files]
@@ -161,9 +164,6 @@ job_test_model = Job(test_model)\
 wf.add_jobs(job_preprocess1,job_preprocess2,job_data_split,job_hpo,job_vgg_model,job_test_model)                                    
 
 try:
-     wf.plan(submit=True)\
-        .wait()\
-        .analyze()\
-        .statistics()
+     wf.plan()
 except PegasusClientError as e:
     print(e.output)
